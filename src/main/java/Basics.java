cca8f8ecf66ffb17767bf87a45a0780657820d4e
ChowdhuryShaswata <shaswata.03@gmail.com>
2023-05-23 00:25:06 -0400

    Updated tasks 1 through 6.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+         int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+         for (int i = 10; i>=0; i = i - 1){
+            System.out.println("Current count: " + i);
+         }
 
 
     }
@@ -142,7 +147,15 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
+        char firstlttr;
+
         // Fill in the rest of the body here
+        String[] lst;
+        lst = to_split.split(" ", 0);
+        for (String j: lst){
+            firstlttr = j.charAt(0);
+            ret.append(firstlttr);
+        }
 
         return ret.toString();
     }
@@ -170,6 +183,14 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        int lngth = arr.length;
+        if (lngth < 1) {
+            return 0;
+        }
+        for (int i = 1; i < lngth; i = i + 2) {
+            current_sum = current_sum + arr[i];
+        }
 
         return current_sum;
     }
